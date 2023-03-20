6b992ec5373e9ca64587a80476b6cf205d4eb913
Rickard Sunden <rickardsunden@gmail.com>
2023-03-20 19:30:31 -0400

    initial commit

---
+++ lib/fire_auth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fire_auth/version"
+
+module FireAuth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fire_auth/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FireAuth
+  VERSION = "0.1.0"
+end
