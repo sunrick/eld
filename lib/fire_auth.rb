fe7d9648390e7323180f45e2f149000394e347f0
Rickard Sunden <rickardsunden@gmail.com>
2023-03-21 19:25:44 -0400

    add dependencies

--- lib/fire_auth.rb
+++ lib/fire_auth.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "fire_auth/version"
+require_relative "fire_auth/cache"
+require_relative "fire_auth/token"
 
 module FireAuth
   class Error < StandardError; end
