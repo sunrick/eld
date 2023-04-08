426d0830d3d0bbf18ef2e0d0f7866c7b75b7ecf7
Rickard Sunden <rickardsunden@gmail.com>
2023-04-07 21:34:00 -0400

    follow other example online

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require "vcr"
 require "pry"
 require "timecop"
 require 'simplecov'
+require "simplecov_json_formatter"
+
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 
 SimpleCov.start
 
