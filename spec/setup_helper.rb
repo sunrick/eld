10ec84d3e3f814431eef4fd4592094aae1ec4bd5
Rickard Sunden <rickardsunden@gmail.com>
2023-04-14 13:42:55 -0400

    rubocop

--- spec/setup_helper.rb
+++ spec/setup_helper.rb
@@ -1,4 +1,6 @@
-RSpec.shared_context 'Setup' do
+# frozen_string_literal: true
+
+RSpec.shared_context "Setup" do
   let(:redis) { Redis.new }
   let(:firebase_id) { "fire-auth-67d5f" }
   let(:current_time) { 1_679_606_440 }
@@ -13,11 +15,11 @@ RSpec.shared_context 'Setup' do
     VCR.use_cassette("google_certificates", allow_playback_repeats: true) do
       Timecop.freeze(Time.at(current_time).utc) do
         FireAuth.cache = if example.metadata[:cache] == :redis
-                            FireAuth::Cache::Redis.new(
-                              client: redis
-                            )
+                          FireAuth::Cache::Redis.new(
+                            client: redis
+                          )
                          else
-                            FireAuth::Cache::Memory.new
+                          FireAuth::Cache::Memory.new
                          end
 
         FireAuth.cache.clear
